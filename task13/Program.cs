bb0c59881dff4db2977a388894605de791ac7ffe
CJINBNH <alex_egorov_89@mail.ru>
2022-08-09 22:36:35 +0400

    Added a solution to the problem using array

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,4 +1,5 @@
-﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿// // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
 Console.Clear();
 int number = new Random().Next(97,103);
 Console.WriteLine($"Вводимое число: {number}");
@@ -10,4 +11,28 @@ if (thirdNumber >= 0 && number >= 100)
 else
 {
     Console.WriteLine("Третья цифра отсутствует");
+}
+
+// // решение с помощью массива
+
+int[] arrayNumbers = { 4, 1106, 23, 566549, 52, 258, 45, 9875, 5, 323 };
+int length = arrayNumbers.Length;
+int index = 0;
+
+while (index < length)
+{
+    string value = arrayNumbers[index].ToString();
+    int a = value.Length;
+    if (a >= 3)
+    {
+        int result = int.Parse(value[2].ToString());
+        Console.Write(" ");
+        Console.Write(result);
+    }
+    else if (a < 3)
+    {
+        Console.Write(" ");
+        Console.Write("Третья цифра отсутствует");
+    }
+    index++;
 }
