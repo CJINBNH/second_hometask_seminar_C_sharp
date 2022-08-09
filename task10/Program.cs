0472a25ca3b161c6dc4f74d75accfedb4409b1ec
CJINBNH <alex_egorov_89@mail.ru>
2022-08-09 20:58:03 +0400

    Completed task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу,
+//которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+int number = new Random().Next(100,1000);
+Console.WriteLine($"Наше трехзначное число: {number}");
+int result = number%100 / 10;
+Console.WriteLine(result);
+
+//решение через конвертирование в строку
+
+string text = number.ToString();
+int middle = int.Parse(text[1].ToString());
+Console.WriteLine($"{middle}");
