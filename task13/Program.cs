46284be04b4125a7eaaaa1d7aeb83b251ed1631d
CJINBNH <alex_egorov_89@mail.ru>
2022-08-09 21:14:48 +0400

    Completed task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+int number = new Random().Next(97,103);
+Console.WriteLine($"Вводимое число: {number}");
+int thirdNumber = number%10;
+if (thirdNumber >= 0 && number >= 100)
+{
+    Console.WriteLine(thirdNumber);
+}
+else
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
