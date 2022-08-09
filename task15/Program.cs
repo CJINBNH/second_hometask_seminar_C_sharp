1c748fc3b537130eaacc840142e316545195dea9
CJINBNH <alex_egorov_89@mail.ru>
2022-08-09 21:26:07 +0400

    Completed task15

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//и проверяет, является ли этот день выходным.
+Console.Clear();
+int dayOfWeek = new Random().Next(1,8);
+Console.WriteLine($"Порядковый номер дня недели: {dayOfWeek}");
+if (dayOfWeek != 6 && dayOfWeek != 7)
+{
+    Console.WriteLine("Это будний день недели");
+}
+else
+{
+    Console.WriteLine("Это выходной");
+}
